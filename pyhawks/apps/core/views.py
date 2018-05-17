c175db89fac5c30b63882073445717d0e94b1fc9
behconsci <behconsci@gmail.com>
2018-05-17 23:07:33 +0200

    added nginx

--- pyhawks/apps/core/views.py
+++ pyhawks/apps/core/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render, HttpResponse
 
 def letsencrypt1(request):
     return HttpResponse(
-        'q6hRTs6iOXlgVgI94gUxh1qTLMuhf7SMm7Zo59Lwwzg.XwnczyaCPkwyyhUn0Hyko_vPtjZBq31fIKmZHsCJqu0',
+        'ABp6xBPOOdxK4W8OMKiqLntaxZDxxnAoFIYHUNWnPag.XwnczyaCPkwyyhUn0Hyko_vPtjZBq31fIKmZHsCJqu0',
         content_type='text/plain'
     )
 
 
 def letsencrypt2(request):
     return HttpResponse(
-        '1qMP4xHZjjjQuwiU8Bjz0-bnsdmsv7VSHPmG8r3ij04.XwnczyaCPkwyyhUn0Hyko_vPtjZBq31fIKmZHsCJqu0',
+        'QvxfKr8WTh3PGyxDxTec_zSAHhiQchDSWeie23rMgwI.XwnczyaCPkwyyhUn0Hyko_vPtjZBq31fIKmZHsCJqu0',
         content_type='text/plain'
     )
 
--- pyhawks/urls.py
+++ pyhawks/urls.py
@@ -6,7 +6,7 @@ from pyhawks.apps.core.views import letsencrypt1, letsencrypt2
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('.well-known/acme-challenge/q6hRTs6iOXlgVgI94gUxh1qTLMuhf7SMm7Zo59Lwwzg', letsencrypt1),
-    path('.well-known/acme-challenge/1qMP4xHZjjjQuwiU8Bjz0-bnsdmsv7VSHPmG8r3ij04', letsencrypt2),
+    path('.well-known/acme-challenge/ABp6xBPOOdxK4W8OMKiqLntaxZDxxnAoFIYHUNWnPag', letsencrypt1),
+    path('.well-known/acme-challenge/QvxfKr8WTh3PGyxDxTec_zSAHhiQchDSWeie23rMgwI', letsencrypt2),
     path('admin/', admin.site.urls),
 ]
